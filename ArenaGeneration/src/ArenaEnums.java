36d7854bc231c741c3ec8dabbda573fcb7141e3c
rksipes <rksipes@eagles.ewu.edu>
2014-07-14 10:54:38 -0700

    Testing commit

---
+++ ArenaGeneration/src/ArenaEnums.java
@@ -0,0 +1,18 @@
+
+public class ArenaEnums {
+
+		public enum Direction {
+			NORTH,
+			SOUTH,
+			EAST,
+			WEST
+		}
+		
+		public enum RoomType {
+			WALL,
+			PATH,
+			DOOR,
+			START,
+			EXIT
+		}
+}
